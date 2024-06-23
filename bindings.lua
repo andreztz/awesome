f0d28e58244875e3a476ceeb4e4d272b53d502c6
André P. Santos <andreztz@gmail.com>
2024-06-23 19:20:01 -0300

    Update key bindings for screen focus

--- bindings.lua
+++ bindings.lua
@@ -42,12 +42,16 @@ local function setup()
 		awful.key({ modkey, "Shift" }, "k", function()
 			awful.client.swap.byidx(-1)
 		end, { description = "swap with previous client by index", group = "client" }),
-		awful.key({ modkey, "Control" }, "j", function()
+
+		-- Move focus between screens
+		awful.key({ modkey, "Control" }, "h", function()
 			awful.screen.focus_relative(1)
 		end, { description = "focus the next screen", group = "screen" }),
-		awful.key({ modkey, "Control" }, "k", function()
+		awful.key({ modkey, "Control" }, "l", function()
 			awful.screen.focus_relative(-1)
 		end, { description = "focus the previous screen", group = "screen" }),
+
+		-- Jump to  urgent client
 		awful.key(
 			{ modkey },
 			"u",
