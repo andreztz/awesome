fe1af054b25c1b7b2be22a6a01127c1cda2be1db
André P. Santos <andreztz@gmail.com>
2025-04-06 00:46:17 -0300

    Ensure layout compatibility with Awesome WM 4.x and newer versions

--- layouts.lua
+++ layouts.lua
@@ -2,7 +2,7 @@ local awful = require("awful")
 
 local function setup()
     -- Tabela de layouts, para cobrir com `awful.layout.inc`, a ordem importa.
-    awful.layout.append_default_layouts({
+    local layouts = {
         awful.layout.suit.floating,
         awful.layout.suit.tile,
         awful.layout.suit.tile.left,
@@ -19,7 +19,14 @@ local function setup()
         awful.layout.suit.corner.ne,
         awful.layout.suit.corner.sw,
         awful.layout.suit.corner.se,
-    })
+    }
+    local success = pcall(function() 
+        awful.layout.append_default_layouts(layouts)
+    end)
+
+    if not success then
+        awful.layout.layouts = layouts
+    end
 end
 
 return {
